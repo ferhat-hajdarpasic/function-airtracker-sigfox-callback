efaafc70ebbcf6739af7ee7cb2281aa7ee86814a
Ferhat Hajdarpasic <ferhat.hajdarpasic@live.com.au>
2018-03-07 19:16:33 +1100

    Using promises.

--- sensors/index.js
+++ sensors/index.js
@@ -1,25 +1,39 @@
+var Connection = require('tedious').Connection;
+var config = {  
+    userName: process.env.DB_USERNAME,  
+    password: process.env.DB_PASSWORD,  
+    server: 'airtracker.database.windows.net',  
+    port: 1433,
+    options: {encrypt: true, database: 'airtracker-sensors-db'}
+};
+var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        context.log('Ferhat:'+ process.env['SQLCONNSTR_airtracker-sensors-db']);
+    });  
+
 module.exports = function (context, req) {
-    context.log('Ferhat:'+ process.env['SQLCONNSTR_airtracker-sensors-db']);
-    var response  = [
-    { 
-    "lat": 51.508515,
-    "lon": -0.125487,
-    "city": "London",
-    "content": "Just some <b>content</b>"
-    },
-    { 
-        "lat": 52.370216,
-    "lon": 4.895168,
-    "city": "Amsterdam",
-    "content": "More <i>content</i>"
-    },
-    { 
-        "lat": 48.856614,
-    "lon": 2.352222,
-    "city": "Paris",
-    "content": "Text here"
-    }
-];
+    if(connection) {
+        context.log('Ferhat:'+ process.env['SQLCONNSTR_airtracker-sensors-db']);
+        var response  = [
+            { 
+            "lat": 51.508515,
+            "lon": -0.125487,
+            "city": "London",
+            "content": "Just some <b>content</b>"
+            },
+            { 
+                "lat": 52.370216,
+            "lon": 4.895168,
+            "city": "Amsterdam",
+            "content": "More <i>content</i>"
+            },
+            { 
+                "lat": 48.856614,
+            "lon": 2.352222,
+            "city": "Paris",
+            "content": "Text here"
+            }
+        ];
         context.res = {
             status: 200,
             body: response,
@@ -27,5 +41,13 @@ module.exports = function (context, req) {
                 'Content-Type': 'application/json'
             }
         };
-    context.done();
-};
+        context.done();
+    } else {
+        context.log('Ferhat:No db connection yet!');
+            context.res = {
+                status: 500,
+                body: 'No db connection yet!'
+            };
+        context.done();    
+    }
+}
