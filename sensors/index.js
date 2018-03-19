00601a796fa263ebe0dffe05672807498fc106f4
fhajdarp <ferhat.hajdarpasic@aon.com>
2018-03-19 15:45:39 +1100

    Sorted out the new functions.

--- sensors/index.js
+++ sensors/index.js
@@ -1,60 +1,43 @@
-var request = require('request');
+var sql = require('mssql');
+
+var db = require('../shared/db.js')();
 
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    var body = {
-        personalizations: [
-            {
-                to: [
-                    {
-                        email: "ferhat.hajdarpasic@live.com.au",
-                        name: "Ferhat Hajdarpasic"
-                    }
-                ],
-                subject: "Hello, World!"
-            }
-        ],
-        from: {
-            email: "fred.hajdarpasic@outlook.com",
-            name: "Fred Hajdarpasic"
-        },
-        reply_to: {
-            email: "fred.hajdarpasic@outlook.com",
-            name: "Fred Hajdarpasic"
-        },
-        subject: "Hello, World!",
-        content: [
-            {
-                type: "text/html",
-                value: "<html><p>Hello, world!</p></html>"
-            }
-        ]
+    context.res = {
+        status: 500,
     };
-
-    if ((req.query.name && req.query.email) ||
-        (req.body.name && req.body.email)) {
-
-        request({
-            url: "https://api.sendgrid.com/v3/mail/send",
-            method: "POST",
-            json: true,
-            body: body
-        }, function (error, response, body) {
-            console.log(response);
-        });
-
-
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    var request = db.request();        
+    if(request) {
+        request.input('device', sql.VarChar, '66')
+            .query('select * from dbo.Sensors')
+            .then(result => {
+                context.log('result.recordset' + JSON.stringify(result.recordset));
+                context.log('result.rowsAffected=' + result.rowsAffected);
+                var response  = [];
+                for(var i = 0; i < result.recordset.length; i++) {
+                    response.push({
+                        device: result.recordset[i].device, 
+                        lat: result.recordset[i].lat,
+                        lng: result.recordset[i].lng,
+                        station: result.recordset[i].lng,
+                        data: result.recordset[i].data
+                    });
+                }
+                context.log('response =' + JSON.stringify(response));
+                context.res = {
+                    status: 200,
+                    body: response,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+                context.done();
+            }).catch(err => {
+                context.log("Catch Error="+ err);
+                context.done();
+            });
+    } else {
+        context.log('Ferhat:No db connection yet!');
+        context.done();    
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
-};
+}
