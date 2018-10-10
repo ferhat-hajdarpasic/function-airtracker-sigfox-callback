e178019679a451779831cbfa0b7428c69692545a
fhajdarp <ferhat.hajdarpasic@aon.com>
2018-10-10 14:02:13 +1100

    First time successful run from within an azure function.

--- downlink/index.js
+++ downlink/index.js
@@ -1,26 +1,54 @@
-const ftp = require('basic-ftp');
-const ftp1 = require('../shared/ftp-sheet.js');
+let ftp = require('../shared/ftp.js');
+let sheet = require('../shared/sheet.js');
+
+async function processCsvFile(filename) {
+    console.log(`Processing ${filename}`);
+    let content = await ftp.download(filename);
+    console.log(content);
+    let [code, name, id, ...rest] = filename.split('-');
+    let sheetName = `${code}-${name}`;
+    console.log(`sheetName=${sheetName}`);
+    let values = [];
+    content.split('\n').forEach(line => {
+        if(line) {
+            console.log(`line=${line}`);
+            values.push([code, name, ...line.split(';')]);
+        }
+    });
+    sheet.write(values);    
+};
 
 module.exports = async function (context, req) {
     context.log(' = ' + JSON.stringify(req.body));
+    context.log('GOOGLE_APPLICATION_CREDENTIALS='+context.executionContext.functionDirectory);
 
-    let files = '';
+    await ftp.connect();
+    await sheet.connect();
+    let errorMessage = 'No error';
+    let files = await ftp.listFiles();
     try {
-        let client = new ftp.Client();
-        await client.access({
-            host: "waws-prod-dm1-081.ftp.azurewebsites.windows.net",
-            user: "carbonblue\\carbonblue",
-            password: "Carbon@Blue",
-            secure: true
-        });
-        files = await client.list();
-        context.log('Connected, files = ' + JSON.stringify(files));
-    } catch(e) {
-        message = e.message;
+        for(i = 0; i < files.length; i++ ) {
+            let filename = files[i].name;
+            if(filename.endsWith('.csv')) {
+                try {
+                await processCsvFile(filename);
+                } catch(error) {
+                    context.log(`Error: ${error}`);
+                }
+                context.log(`Processing successful, removing ${filename}`);
+                await ftp.remove(filename);
+                context.log(`Removing ${filename} successful`);
+            }
+        }
+        ftp.close();
+    } catch(error) {
+        context.log(`Error: ${error}`);
+        errorMessage = error;
     }
+
     context.res = {
         body: {
-            files: files
+            error: process.env.GOOGLE_APPLICATION_CREDENTIALS
         }
     };
     context.done();
